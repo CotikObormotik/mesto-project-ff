df06951d88bba14dd9fa114fc71ade4724e9c184
M.Cat <kif24rus@mail.ru>
2024-02-04 00:00:11 +0600

    Cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+    const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+    const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 
+    function creatCArd (initialCards, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = initialCards.link;
+    cardElement.querySelector('.card__title').textContent = initialCards.name;
+
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+
 // @todo: Функция удаления карточки
+    function deleteCard(cardElement) {
+        cardElement.remove();
+    }
+    return cardElement;
+};
+
 
 // @todo: Вывести карточки на страницу
+ initialCards.forEach((element) => {
+    placesList.append(creatCArd(element));
+ });
