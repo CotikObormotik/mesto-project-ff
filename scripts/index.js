c87830febb19283066d9242325e77715f567c9f4
M.Cat <kif24rus@mail.ru>
2024-02-04 21:27:49 +0600

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@
     const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 
-    function creatCArd (initialCards, deleteCard) {
+    function creatCard (initialCards) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
 
@@ -14,16 +14,16 @@
     deleteButton.addEventListener('click', () => {
         deleteCard(cardElement);
     });
-
-// @todo: Функция удаления карточки
-    function deleteCard(cardElement) {
-        cardElement.remove();
-    }
     return cardElement;
 };
 
-
 // @todo: Вывести карточки на страницу
  initialCards.forEach((element) => {
-    placesList.append(creatCArd(element));
+    placesList.append(creatCard(element));
  });
+ 
+// @todo: Функция удаления карточки
+
+ function deleteCard(cardElement) {
+    cardElement.remove();
+};
